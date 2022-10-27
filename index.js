647c6b29e7751af3583ecac50dde2d07d3120dcf
Prachi Bala <prachibala330@gmail.com>
2022-10-27 15:17:07 +0600

    ver-04

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 const port = process.env.port || 5000;
+const allCourses = require("./Data/course.json");
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.get("/allCourses", (req, res) => {
+    res.send(allCourses);
+});
